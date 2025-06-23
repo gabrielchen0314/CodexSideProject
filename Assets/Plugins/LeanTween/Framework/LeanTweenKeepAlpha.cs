6a1b2bdb715aae33d4bb592b80d720a0133dea58
2917_陳鼎翰 <gabrielchen0314@gmail.com>
2025-06-23 14:47:15 +0800

    LeanTween 上傳

---
+++ Assets/Plugins/LeanTween/Framework/LeanTweenKeepAlpha.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class LeanTweenKeepAlpha : MonoBehaviour
+{
+    public float AlphaValue = 1;
+
+    public void SetAlphaValue(float iValue)
+    {
+        AlphaValue = iValue;
+    }
+}
